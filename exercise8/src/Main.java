69d08126add7ba481cc41bc44491bf1282919897
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2024-10-19 00:33:25 +0400

    Add new folder - exercise 8

---
+++ exercise8/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        public class Main {
+            public static void main(String[] args) {
+                int S = 3665;
+
+                int hours = S / 3600;
+                int minutes = (S - hours * 3600) / 60;
+                int seconds = S - (hours * 3600 + minutes * 60);
+
+                System.out.println(hours + ":" + minutes + ":" + seconds);
+            }
+        }
+    }
+}
