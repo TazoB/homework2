c778feb14e60dbcc246dab2f1fb248ae98677f1d
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2024-10-19 00:00:43 +0400

    Add new folder - exercise3

---
+++ exercise3/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 1000;
+        if(x >= 100 && x <= 999){
+            System.out.println("სამნიშნა რიცხვია.");
+        }else{
+            System.out.println("არ არის სამნიშნა რიცხვი.");
+        }
+    }
+}
